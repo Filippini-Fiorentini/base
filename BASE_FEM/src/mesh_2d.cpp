2eee691a1d7e7d98c7eab2c63c0cbce869696af5
LoreFio <lorenzo1.fiorentini@mail.polimi.it>
2018-04-06 11:01:24 +0000

    Correct mesh at the boundary

--- BASE_FEM/src/mesh_2d.cpp
+++ BASE_FEM/src/mesh_2d.cpp
@@ -5,7 +5,7 @@ namespace fem
 
 Mesh_2d::Mesh_2d(Real limit_x, Real limit_y): 
     L_x(limit_x), L_y(limit_y), nodes(PTS_MESH_X*PTS_MESH_Y), tr_indices( 2 * (PTS_MESH_X - 1)  * (PTS_MESH_Y - 1)),
-    nr_rows(PTS_MESH_Y), nr_cols(PTS_MESH_X), h_x(limit_x/PTS_MESH_X), h_y(limit_y/PTS_MESH_Y)
+    nr_rows(PTS_MESH_Y), nr_cols(PTS_MESH_X), h_x( limit_x/(PTS_MESH_X-1) ), h_y( limit_y/(PTS_MESH_Y-1) )
     {
     nln = (degree+1)*(degree+2)/2;  
     // Building the nodes 
@@ -22,13 +22,13 @@ Mesh_2d::Mesh_2d(Real limit_x, Real limit_y):
     // Building the triangles 
     for(std::size_t i = 0; i < (PTS_MESH_X-1) * (PTS_MESH_Y-1); ++i)
         {
-        tr_indices[counter_tri++] = {i,     i + 1,              i + PTS_MESH_X};
+        tr_indices[counter_tri++] = {i    , i + 1             , i + PTS_MESH_X};
         tr_indices[counter_tri++] = {i + 1, i + 1 + PTS_MESH_X, i + PTS_MESH_X};
         }
     }
 
 Mesh_2d::Mesh_2d(Real limit_x, Real limit_y, const std::vector<Node_2d>& mesh, std::size_t rows, std::size_t cols, std::size_t deg):
-	L_x(limit_x), L_y(limit_y), nodes(mesh), nr_rows(rows), nr_cols(cols), h_x(limit_x/nr_cols), 
+	L_x(limit_x), L_y(limit_y), nodes(mesh), nr_rows(rows>= 2 ? rows:2), nr_cols(cols>= 2 ? cols : 2), h_x(limit_x/nr_cols), 
 	        h_y(limit_y/nr_rows), degree(deg) 
 	{
 	nln = (degree+1)*(degree+2)/2;
