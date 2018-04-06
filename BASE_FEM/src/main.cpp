47f20529f528b6f5e54e0b36451d6b4247bee501
LoreFio <lorenzo1.fiorentini@mail.polimi.it>
2018-04-06 08:39:44 +0200

    again

--- BASE_FEM/src/main.cpp
+++ BASE_FEM/src/main.cpp
@@ -3,50 +3,50 @@
 
 struct f1
 	{
-	fdm::Real operator()(fdm::Real x, fdm::Real y)
+	fem::Real operator()(fem::Real x, fem::Real y)
 		{return 8*M_PI*M_PI*sin(2*M_PI*x)*sin(2*M_PI*y);}
 	};
 
 struct exact_1
 	{
-	fdm::Real operator()(fdm::Real x, fdm::Real y)
+	fem::Real operator()(fem::Real x, fem::Real y)
 			{return sin(2*M_PI*x)*sin(2*M_PI*y);}		
 	};
 
 int main (int argc, char* argv [])
 	{
 	// MESH TEST
-	std::vector<std::string> files = fdm::input_list_file("non_esisto.txt", argc, argv);
+	std::vector<std::string> files = fem::input_list_file("non_esisto.txt", argc, argv);
 
-    fdm::Real Lx = fdm::read_value(files[fdm::IN], "Lx", 10.0);
-    fdm::Real Ly = fdm::read_value(files[fdm::IN], "Ly", 10.0);
+    fem::Real Lx = fem::read_value(files[fem::IN], "Lx", 10.0);
+    fem::Real Ly = fem::read_value(files[fem::IN], "Ly", 10.0);
     
-    fdm::Mesh_2d mesh(Lx,Ly);
+    fem::Mesh_2d mesh(Lx,Ly);
         
-    std::ofstream dbg(files[fdm::DBG]);
+    std::ofstream dbg(files[fem::DBG]);
         
     mesh.print(dbg);
     
     // SOLVER TEST    
-	std::vector<std::function<fdm::Real(fdm::Real, fdm::Real)>> vect_functions;
+	std::vector<std::function<fem::Real(fem::Real, fem::Real)>> vect_functions;
 	vect_functions.emplace_back(f1());
 	vect_functions.emplace_back(exact_1());
-	vect_functions.emplace_back(fdm::function_2d_0());
-	vect_functions.emplace_back(fdm::function_2d_1());
+	vect_functions.emplace_back(fem::function_2d_0());
+	vect_functions.emplace_back(fem::function_2d_1());
 	
-	fdm::Fdm_2d_solver solver_1(mesh, vect_functions[0], vect_functions[2], 
+	fem::Fdm_2d_solver solver_1(mesh, vect_functions[0], vect_functions[2], 
 	            vect_functions[1]);
-	Eigen::Matrix<fdm::Real, Eigen::Dynamic, 1> u = solver_1.solve();
+	Eigen::Matrix<fem::Real, Eigen::Dynamic, 1> u = solver_1.solve();
 	//std::cout << "result:" << std::endl << u << std::endl;
 	std::cout << "result calculated" << std::endl;
 	
 	
-	Eigen::Matrix<fdm::Real, Eigen::Dynamic, 1> exact_solution = solver_1.exact_sol();
-	Eigen::Matrix<fdm::Real, Eigen::Dynamic, 1> diff = u-exact_solution;
+	Eigen::Matrix<fem::Real, Eigen::Dynamic, 1> exact_solution = solver_1.exact_sol();
+	Eigen::Matrix<fem::Real, Eigen::Dynamic, 1> diff = u-exact_solution;
 	
-	fdm::Real norml2 = diff.norm();
+	fem::Real norml2 = diff.norm();
 	std::cout << "l2 norm of the difference:" << norml2 << std::endl;	
 	
-	write_solution_file(mesh, u, files[fdm::SOL]);
-	write_solution_file(mesh, exact_solution, files[fdm::EXACT]);
+	write_solution_file(mesh, u, files[fem::SOL]);
+	write_solution_file(mesh, exact_solution, files[fem::EXACT]);
 	}
